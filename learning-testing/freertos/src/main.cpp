02a672c6a1cb92cfb0ac5be6e2546f9bca1edbb6
✞dangerøse✞ <dangerose777@gmail.com>
2026-01-24 14:42:06 +0100

    testing code
    - 1 led with button
    - 2 leds blinking
    testing without freertos

--- learning-testing/freertos/src/main.cpp
+++ learning-testing/freertos/src/main.cpp
@@ -1,19 +1,34 @@
 #include <Arduino.h>
 
 // PIN DEFINITIONS
-const int ledPin = 4;
+const int ledPinBlue = 4;
+const int ledPinRed = 18;
+const int ledPinGreen = 21;
+
+const int buttonPin = 27;
+int buttonState = 0;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("FreeRTOS Tests");
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPinBlue, OUTPUT);
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // Serial.println("Led ON");
-  // digitalWrite(ledPin, HIGH);
-  // delay(1000);
-  // Serial.println("Led OFF");
-  // digitalWrite(ledPin, LOW);
-  // delay(1000);
+  digitalWrite(ledPinBlue, HIGH);
+  digitalWrite(ledPinRed, LOW);
+  delay(1000);
+  digitalWrite(ledPinBlue, LOW);
+  digitalWrite(ledPinRed, HIGH);
+  delay(1000);
+
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == LOW) {
+    digitalWrite(ledPinGreen, HIGH);
+  } else {
+    digitalWrite(ledPinGreen, LOW);
+  }
 }
